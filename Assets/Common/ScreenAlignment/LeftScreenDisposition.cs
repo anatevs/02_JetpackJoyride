88ed05fac5bba77d28a24833bf88c7cbe8405db2
anatevs <anatevs@gmail.com>
2025-02-10 18:31:23 +0300

    setup map sections

---
+++ Assets/Common/ScreenAlignment/LeftScreenDisposition.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameCore
+{
+    public class LeftScreenDisposition : MonoBehaviour
+    {
+        private List<ILeftScreenAlignment> _leftScreenAlignments;
+
+        private Camera _camera;
+
+        private float _leftCameraBorder;
+
+        public void Construct(List<ILeftScreenAlignment> leftScreenAlignments)
+        {
+            _leftScreenAlignments = leftScreenAlignments;
+        }
+
+        private void Start()
+        {
+            _camera = Camera.main;
+
+            _leftCameraBorder = GetLeftCameraBorder();
+
+            foreach(var alignment in _leftScreenAlignments)
+            {
+                alignment.AlignXToScreen(_leftCameraBorder);
+            }
+        }
+
+        public void InitPositions()
+        {
+            var leftCameraBorder = GetLeftCameraBorder();
+
+            if (_leftCameraBorder == leftCameraBorder)
+            {
+                foreach (var alignment in _leftScreenAlignments)
+                {
+                    alignment.SetToInitPosX();
+                }
+            }
+            else
+            {
+                _leftCameraBorder = leftCameraBorder;
+
+                foreach (var alignment in _leftScreenAlignments)
+                {
+                    alignment.AlignXToScreen(leftCameraBorder);
+                }
+            }
+        }
+
+        private float GetLeftCameraBorder()
+        {
+            return _camera.transform.position.x
+                - _camera.aspect * _camera.orthographicSize;
+        }
+    }
+}
---
+++ Assets/Enviroment/Scripts/Tiles/MapSection.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace GameCore
+{
+    public sealed class MapSection : MonoBehaviour
+    {
+        public event Action OnBorderAchieved;
+
+        public event Action<int> OnInitPosSet;
+
+        public float LeftBorderShift => _leftBorderShift;
+
+        public float LeftCameraBorder
+        {
+            get => _leftCameraBorder;
+            set => _leftCameraBorder = value;
+        }
+
+        [SerializeField]
+        private Tilemap _sectionTilemap;
+
+        private float _rightBorderShift;
+
+        private float _leftBorderShift;
+
+        private float _leftCameraBorder;
+
+        private void Awake()
+        {
+            _rightBorderShift = (_sectionTilemap.size.x
+                + _sectionTilemap.origin.x) * _sectionTilemap.cellSize.x;
+
+            _leftBorderShift = -_sectionTilemap.origin.x * _sectionTilemap.cellSize.x;
+        }
+
+        private void Update()
+        {
+            if (GetRightBorderX() <= _leftCameraBorder)
+            {
+                OnBorderAchieved?.Invoke();
+            }
+        }
+
+        public void PlaceLeftBorderToX(float x)
+        {
+            transform.position = new Vector3(
+                x + _leftBorderShift,
+                transform.position.y,
+                transform.position.z);
+        }
+
+        public float GetRightBorderX()
+        {
+            return transform.position.x + _rightBorderShift;
+        }
+
+        public void InvokeOnInitPosSet(int mapOrder)
+        {
+            OnInitPosSet?.Invoke(mapOrder);
+        }
+    }
+}
